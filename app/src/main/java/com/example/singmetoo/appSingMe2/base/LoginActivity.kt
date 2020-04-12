cc59ddeb74c2332b090bedba17edbb1b2bcf6920
Admin <himanshu24yadav@gmail.com>
2020-04-12 21:05:44 +0530

    Login Screen

---
+++ app/src/main/java/com/example/singmetoo/appSingMe2/base/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.singmetoo.appSingMe2.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.singmetoo.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_login_screen)
+    }
+}
